80fe7825f1274e1eb5a8e061912044257909e3c9
jrenzzo <u202118568@upc.edu.pe>
2024-05-13 19:46:22 -0500

    feat: add token service and auth dto

---
+++ Security/Domain/Services/TokenService.cs
@@ -0,0 +1,46 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Users.Domain.Model;
+
+namespace Security.Domain.Services.Communication
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<User> _userManager;
+        public TokenService(UserManager<User> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email), 
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSettings:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            var tokenOptions = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+    }
+}
