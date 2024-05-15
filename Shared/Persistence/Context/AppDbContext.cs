dbd0d9180616d7be2648f33f1d32dbce09be2c6d
jrenzzo <u202118568@upc.edu.pe>
2024-05-14 23:53:48 -0500

    feat: add persistence for devices and repository implementation

--- Shared/Persistence/Context/AppDbContext.cs
+++ Shared/Persistence/Context/AppDbContext.cs
@@ -9,6 +9,8 @@ namespace Shared.Persistence.Context;
 public class AppDbContext : IdentityDbContext<User, Role, int>
 {
     public AppDbContext(DbContextOptions options) : base(options){}
+
+    public DbSet<Device> Devices { get; set; }
     
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -18,6 +20,15 @@ public class AppDbContext : IdentityDbContext<User, Role, int>
                 new Role {Id = 1, Name = "User", NormalizedName = "USER" },
                 new Role {Id = 2, Name = "Admin", NormalizedName = "ADMIN" }
             );
+
+        //Customers
+        builder.Entity<Device>().ToTable("Devices");
+        builder.Entity<Device>().HasKey(p => p.Id);
+        builder.Entity<Device>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Device>().Property(p => p.Name).IsRequired().HasMaxLength(200);
+        builder.Entity<Device>().Property(p => p.Info).IsRequired().HasMaxLength(600);
+        builder.Entity<Device>().Property(p => p.UserType).IsRequired();
+        builder.Entity<Device>().Property(p => p.UserId).IsRequired();
 
         //Snake Case Conventions
         
