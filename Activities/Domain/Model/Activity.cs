59da94b3b5a5b83f31eea9eae64886b071ee93cb
jrenzzo <u202118568@upc.edu.pe>
2024-05-27 15:49:00 -0500

    feat: add activity entity

---
+++ Activities/Domain/Model/Activity.cs
@@ -0,0 +1,17 @@
+using System;
+using Users.Domain.Model;
+
+namespace Activites.Domain.Model;
+
+public class Activity 
+{
+    // Properties
+    public int Id { get; set; }
+    public string AppName { get; set; }
+    public TimeSpan TotalTimeUsedPerDay { get; set; }
+    public DateTime DateReported { get; set; }
+    public int TotalNotifications { get; set; }
+    public int UserId { get; set; }
+    public Device Device {get; set;}
+    public int DeviceId { get; set; }
+}
