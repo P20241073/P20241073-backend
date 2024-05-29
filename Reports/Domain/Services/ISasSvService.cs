c209ae66279b4de760f4a11161eb70c5b66a0621
jrenzzo <u202118568@upc.edu.pe>
2024-05-29 13:04:39 -0500

    feat: implement service layer for sassv

--- Reports/Domain/Services/ISasSvService.cs
+++ Reports/Domain/Services/ISasSvService.cs
@@ -6,7 +6,7 @@ public interface ISasSvService
 {
     Task<IEnumerable<SasSv>> ListAsync();
     Task<SasSvResponse> GetByIdAsync(int id);
-    Task<IEnumerable<SasSv>> GetByDeviceIdAsync(int userId);
+    Task<IEnumerable<SasSv>> GetByDeviceIdAsync(int deviceId);
     Task<SasSvResponse> SaveAsync(SasSv SasSv);
     Task<SasSvResponse> UpdateAsync(int id, SasSv SasSv);
     Task<SasSvResponse> DeleteAsync(int id);
